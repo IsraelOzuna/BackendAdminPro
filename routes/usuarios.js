48e2e761e100f533bf5aecb362ef9dfa29630981
Israel Reyes Ozuna <iro1904@hotmail.com>
2021-09-01 21:13:45 -0500

    Seccion para Auth de usuario, CRUD de usuarios tambien agregada

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,12 +3,40 @@
 */
 
 const { Router } = require('express');
-const { getUsuarios, crearUsuario } = require('../controllers/usuarios')
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+
+const { getUsuarios, crearUsuario, actualizarUsuario, borrarUsuario } = require('../controllers/usuarios');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
-router.get('/', getUsuarios);
+router.get('/', validarJWT, getUsuarios);
 
-router.post('/', crearUsuario);
+router.post('/',
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('password', 'La contrasena es obligatoria').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        validarCampos
+    ],
+    crearUsuario
+);
+
+router.put('/:id',
+    [
+        validarJWT,
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('role', 'El rol es obligatorio').not().isEmpty(),
+        validarCampos,
+    ],
+    actualizarUsuario
+);
+
+router.delete('/:id',
+    validarJWT,
+    borrarUsuario
+);
 
 module.exports = router;
